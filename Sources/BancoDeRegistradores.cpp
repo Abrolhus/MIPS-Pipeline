2ad27a827703482fcb35e728882ba3648621480c
Samuel Abreu <samuelapp@outlook.com>
2021-08-19 15:30:24 -0300

    feat: add BancoDERegistradores class, with read and write methods

---
+++ Sources/BancoDeRegistradores.cpp
@@ -0,0 +1,17 @@
+//
+// Created by abrolhus on 19/08/2021.
+//
+
+#include "../Headers/BancoDeRegistradores.h"
+
+BancoDeRegistradores::BancoDeRegistradores(int size) {
+    this->regs = new std::vector<int>(size);
+    this->regs.at(0) = 0;
+}
+
+int BancoDeRegistradores::read(int reg) const {
+    return this->regs.at(reg);
+}
+void BancoDeRegistradores::write(int reg, int word) {
+    this->regs.at(reg) = word;
+}
